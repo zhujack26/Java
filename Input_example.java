6d61dbda9a5b14ec98b6b4538595750a86e508c7
JS <zhujack26@gmail.com>
2023-10-23 10:13:31 +0900

    TIL : 20231023

---
+++ Input_example.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.println(A);
+        System.out.println(B);
+    }
+}
+
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputArray = br.readLine().split(" ");
+        int A = Integer.parseInt(inputArray[0]);
+        int B = Integer.parseInt(inputArray[1]);
+        System.out.println(A);
+        System.out.println(B);
+    }
+}
+
+
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(A);
+        System.out.println(B);
+    }
+}
---
+++ 문자열 분리 - split() 와 StringTokenizer 의 차이점.md
@@ -0,0 +1,9 @@
+### 문자열 분리 - split() 와 StringTokenizer 의 차이점
+
+
+
+[Oh! My Library :: 문자열 분리 - split() 와 StringTokenizer 의 차이점](https://library1008.tistory.com/16)
+
+[[Java] StringTokenizer 문자열 분리하기 (split과 차이는 뭘까?)](https://dev-coco.tistory.com/94)
+
+
